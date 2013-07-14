89af440c1e82c5541ce2992ef1a2c3c21e7075bf
Pedro Ferreira <plferreira4@gmail.com>
2013-07-15 00:51:08 +0100

    - tell the average of the occurrences - load the username/password/server/channel from an external configuration file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # python2.7
 
 import sys
+import json
 
 from twisted.words.protocols import irc
 from twisted.internet import protocol
@@ -8,12 +9,20 @@ from twisted.internet.task import LoopingCall
 from twisted.internet import reactor
 
 
+
 class Bot( irc.IRCClient ):
 
-    def _get_nickname( self ):
-        return self.factory.nickname
 
-    nickname = property( _get_nickname )
+    def _get_username( self ):
+        return self.factory.username
+
+    def _get_password( self ):
+        return self.factory.password
+
+
+    username = property( _get_username )
+    nickname = username
+    password = property( _get_password )
 
 
     def signedOn( self ):
@@ -30,6 +39,8 @@ class Bot( irc.IRCClient ):
 
         self.count_occurrences = 0
         self.word_to_count = 'something'
+        self.total_rounds = 0
+        self.total_count_occurrences = 0
 
         LoopingCall( self.printOccurrencesPerMinute ).start( 10, now= False )
 
@@ -45,12 +56,13 @@ class Bot( irc.IRCClient ):
             return
 
             # private message to the bot
-        if channel == self.nickname:
-            self.msg( self.factory.channel, 'hello there' )
+        #if channel == self.nickname:
+        #    self.msg( self.factory.channel, 'hello there' )
 
 
         if self.nickname in message:
             self.msg( self.factory.channel, message )
+
 
         self.count_occurrences += message.count( self.word_to_count )
 
@@ -59,9 +71,18 @@ class Bot( irc.IRCClient ):
 
     def printOccurrencesPerMinute( self ):
 
-        self.msg( self.factory.channel, '{} Per Minute: {}'.format( self.word_to_count, self.count_occurrences ) )
+        self.total_count_occurrences += self.count_occurrences
+        self.total_rounds += 1
+
+        average = float( self.total_count_occurrences ) / float( self.total_rounds )
+
+
+        self.msg( self.factory.channel, '{} Per Minute: {} // Average: {}'.format( self.word_to_count, self.count_occurrences, average ) )
 
         self.count_occurrences = 0
+
+
+
 
 
 
@@ -69,16 +90,18 @@ class BotFactory( protocol.ClientFactory ):
 
     protocol = Bot
 
-    def __init__( self, channel, nickname= 'bot' ):
+    def __init__( self, channel, username, password ):
 
         self.channel = channel
-        self.nickname = nickname
-
+        self.username = username
+        self.password = password
 
 
     def clientConnectionLost( self, connector, reason ):
 
         print 'Lost Connection {}, reconnecting.'.format( reason )
+        print reason.printTraceback()
+
         connector.connect()
 
 
@@ -90,6 +113,18 @@ class BotFactory( protocol.ClientFactory ):
 
 if __name__ == '__main__':
 
-    channel = sys.argv[1]
-    reactor.connectTCP( 'irc.freenode.net', 6667, BotFactory('#' + channel), timeout= 5 )
+    with open( 'config.json', 'r' ) as f:
+        content = f.read()
+
+    contentJson = json.loads( content )
+
+        # twisted doesn't like unicode
+    username = str( contentJson[ 'username' ] )
+    password = str( contentJson[ 'password' ] )
+    server = str( contentJson[ 'server' ] )
+    channel = str( contentJson[ 'channel' ] )
+
+
+
+    reactor.connectTCP( server, 6667, BotFactory( channel, username, password ), timeout= 2 )
     reactor.run()

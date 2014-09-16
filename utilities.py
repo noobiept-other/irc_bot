020c947bc4d09e5d583c4fe35ee47c5d131e072e
Pedro Ferreira <plferreira4@gmail.com>
2014-09-16 16:25:34 +0100

    - add a better message to TimePassed

--- utilities.py
+++ utilities.py
@@ -15,10 +15,8 @@ class TimePassed:
         difference = current - self.initial_time
 
         totalSeconds = difference.total_seconds()
-        #HERE have a better string
 
-
-        return str( difference )
+        return get_time_string( totalSeconds )
 
 
 
@@ -48,3 +46,70 @@ def fromUnicodeToStr( config ):
 
 
     return config
+
+
+
+def get_time_string( totalSeconds ):
+    """
+        Converts seconds to days/hours/minutes/seconds
+
+    :param totalSeconds: int
+    :return: str
+    """
+
+    try:
+        totalSeconds = int( totalSeconds )
+
+    except ValueError:
+        return totalSeconds
+
+
+        # we work in seconds
+    minute = 60
+    hour = 60 * minute
+    day = 24 * hour
+
+    minuteCount = 0
+    hourCount = 0
+    dayCount = 0
+
+        # count the days
+    while totalSeconds >= day:
+        dayCount += 1
+        totalSeconds -= day
+
+        # count the hours
+    while totalSeconds >= hour:
+        hourCount += 1
+        totalSeconds -= hour
+
+        # count the minutes
+    while totalSeconds >= minute:
+        minuteCount += 1
+        totalSeconds -= minute
+
+    secondCount = int( round( totalSeconds, 0 ) )
+
+    def addUnit( dateStr, value, unit ):
+
+        if value != 0:
+            if value > 1:
+                unit += 's'
+
+                # first unit we add, don't need to add a space between the previous date string
+            if dateStr == '':
+                return '{} {}'.format( value, unit )
+
+            else:
+                return '{} {} {}'.format( dateStr, value, unit )
+
+        else:
+            return dateStr
+
+
+    time = addUnit( '', dayCount, 'day' )
+    time = addUnit( time, hourCount, 'hour' )
+    time = addUnit( time, minuteCount, 'minute' )
+    time = addUnit( time, secondCount, 'second' )
+
+    return time
